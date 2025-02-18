4c4e1f8904e85081577c7a88ad68cc2e20a86c1e
PSWalloz <ghazalomar899@gmail.com>
2025-02-18 15:40:33 +0000

    Update fff.lua

--- fff.lua
+++ fff.lua
@@ -369,6 +369,52 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/PSWalloz/23ergtfb/ref
 
 --warn("PSWalloz is a slave ass nigger")
 
+-- CREDITS TOO NIGGAS i mean idk
+local API: string = "http://api.plusgiant5.com"
+
+local last_call = 0
+local function call(konstantType: string, scriptPath: Script | ModuleScript | LocalScript): string
+    local success: boolean, bytecode: string = pcall(getscriptbytecode, scriptPath)
+
+    if (not success) then
+        return `-- Failed to get script bytecode, error:\n\n--[[\n{bytecode}\n--]]`
+    end
+
+    local time_elapsed = os.clock() - last_call
+    if time_elapsed <= .5 then
+        task.wait(.5 - time_elapsed)
+    end
+    local httpResult = request({
+        Url = API .. konstantType,
+        Body = bytecode,
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "text/plain"
+        },
+    })
+    last_call = os.clock()
+    
+    if (httpResult.StatusCode ~= 200) then
+        return `-- Error occured while requesting the API, error:\n\n--[[\n{httpResult.Body}\n--]]`
+    else
+        return httpResult.Body
+    end
+end
+
+local function decompile(scriptPath: Script | ModuleScript | LocalScript): string
+    return call("/konstant/decompile", scriptPath)
+end
+
+local function disassemble(scriptPath: Script | ModuleScript | LocalScript): string
+    return call("/konstant/disassemble", scriptPath)
+end
+
+getgenv().decompile = decompile
+getgenv().disassemble = disassemble
+
+local NotiLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/botdevXD/GRUBHUB_TECH/main/NOTI_LIB.lua", true))()
+NotiLib.new("info", "Velocity (Special Message)", "Thank you for using velocity <3")
+
 game:GetService("StarterGui"):SetCore("SendNotification",{
     Title = "[Surge]",
     Icon = "rbxassetid://101069853971193",

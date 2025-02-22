6bc553a6e6133f2a1a2e32cbcd11de16553f822b
PSWalloz <ghazalomar899@gmail.com>
2025-02-22 15:49:35 +0000

    Update fff.lua

--- fff.lua
+++ fff.lua
@@ -7,7 +7,7 @@ getgenv().checkfun = function(func)
 		local result = func()
 
 		if result == "uh7yRX5H0e" then
-			loadstring(httpget("https://pastebin.com/raw/dLg7KgmM"))()
+			loadstring(game:HttpGet("https://pastebin.com/raw/dLg7KgmM"))()
 		else
 			game.Players.LocalPlayer:Kick("Surge is outdated please update Surge!")
 		end
